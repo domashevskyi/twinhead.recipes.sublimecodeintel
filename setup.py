5c8f1229f2f0c26d23f6f0b0d6e455931f83fecb
Dmitriy Domashevskiy <dmitriy.domashevskiy@gmail.com>
2012-02-26 10:13:29 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import os
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+name = 'pb.recipes.pydev'
+entry_points = {'zc.buildout': ['default = %s:Sublime' % name]}
+
+
+setup(
+    name=name,
+    description="Generates SublimeCodeInt configuration file with all necessary extra paths",
+    long_description=(
+                       read('README.txt')
+                       + "\n\n" +
+                       read('CHANGES.txt')
+    ),
+    version='0.1.0',
+    author="Dmitriy Domashevskiy",
+    author_email="dmitriy.domashevskiy@gmail.com",
+    license="BSD",
+    keywords="buildout recipe Sublime",
+    url='https://github.com/domashevskiy/twinhead.recipes.sublime',
+    packages=find_packages('src'),
+    include_package_data=True,
+    package_dir={'': 'src'},
+    namespace_packages=['twinhead', 'twinhead.recipes'],
+    install_requires=['setuptools',
+                        'zc.buildout',
+                        'zc.recipe.egg',
+                        ],
+    test_suite=name + '.tests.test_suite',
+    entry_points=entry_points,
+    zip_safe=True,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Build Tools",
+        ]
+    )
